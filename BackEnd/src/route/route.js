cc0eac93d180e1c6feda4df23b80b5ae6c9c8d0a
Harsh25081 <harsh.pataudi258@gmail.com>
2023-02-13 00:33:37 +0530

    done project

--- BackEnd/src/route/route.js
+++ BackEnd/src/route/route.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cron = require("node-cron")
 const router = express.Router()
 
-cron.schedule('* * * * *', () => {
+cron.schedule('* * * *', () => {
     check();
   });
 
